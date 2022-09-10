ccd76c29eec4313b6c879f4637cb692173784a49
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-10 14:33:33 +0300

    Added update operation to CityService.

--- src/main/java/com/bbkdevelopment/cityservice/service/CityService.java
+++ src/main/java/com/bbkdevelopment/cityservice/service/CityService.java
@@ -1,5 +1,6 @@
 package com.bbkdevelopment.cityservice.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -20,6 +21,14 @@ public class CityService {
 
     public City createCity(City city) {
         return cityRepository.save(city);
+    }
+
+    public void updateCity(String id, City city) {
+        City oldCity = getCityById(id);
+        oldCity.setName(city.getName());
+        oldCity.setCreationDate(new Date());
+
+        cityRepository.save(oldCity);
     }
 
     public void deleteCity(String id) {
